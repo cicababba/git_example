744baccbf60edad538739b4e766ddebf434db664
cicababba <45292355+cicababba@users.noreply.github.com>
2021-10-19 11:20:05 +0200

    Update utils.js

--- shared/utils.js
+++ shared/utils.js
@@ -3,6 +3,11 @@ const sendErrorResponse = (httpCode, error, res) => {
     res.status(httpCode).send({status: "KO", message: `Something went wrong, error caused by: ${error}`})
 }
 
+const sendSuccessResponseTestDaGithub = (httpCode, result, res) => {
+    console.log("sto cercando di creare un conflitto")
+    res.status(httpCode).send({status: "OK", result})
+}
+
 const sendSuccessResponse = (httpCode, result, res) => {
     res.status(httpCode).send({status: "OK", result})
 }
