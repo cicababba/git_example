076e51ecd90f87fa72aede3f5cb4d8acc9088b1b
cicababba <45292355+cicababba@users.noreply.github.com>
2021-11-03 11:11:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,11 @@ mongoose.connect(dbURI).then(() => {
     console.error(`Something went wrong, error: ${err}`)
 })
 
-app.listen(3000)
+app.listen(7777)
+//app.listen(7777)
+//app.listen(7777)
+//app.listen(7777)
+//app.listen(7777)
 app.use(express.json())
 app.use('/restaurant', restaurantRoutes)
 app.use('/auth', authRoutes)
